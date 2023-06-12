4392ecc4950be915714774d219694c7686d18725
alaakamal333 <kamiliahazem77@gmail.com>
2023-06-12 17:02:35 +0300

    loli

--- login project/connection.php
+++ login project/connection.php
@@ -1,13 +1,18 @@
 <?php
-function OpenConnection()
-{
-	$serverName = "cure4soul.database.windows.net";
-	$connectionOptions = array("Database"=>"Cure4soul",
-		"Uid"=>"cur4soul", "PWD"=>"AdminAdmin123");
-	$conn = sqlsrv_connect($serverName, $connectionOptions);
-	if($conn == false)
-		die(FormatErrors(sqlsrv_errors()));
-
-	return $conn;
+if (!function_exists('OpenConnection')) {
+    function OpenConnection()
+    {
+        $serverName = "cure4soul.database.windows.net";
+        $connectionOptions = array(
+            "Database" => "Cure4soul",
+            "Uid" => "cur4soul",
+            "PWD" => "AdminAdmin123"
+        );
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if ($conn === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+        return $conn;
+    }
 }
 ?>
